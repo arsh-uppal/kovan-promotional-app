4c259d865b5acfce7357b64b580934328c401f9e
Arsh <arshdeepgary@gmail.com>
2020-07-07 18:55:36 -0700

    style: adds a grid to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
-const Home = () => {
-  return <div>Home</div>;
-};
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  heroContainer: {
+    minHeight: '80vh',
+    backgroundColor: 'yellow',
+  },
+  heroImgContainer: {
+    backgroundColor: 'red',
+  },
+  heroContentContainer: {
+    backgroundColor: 'blue',
+  },
+}));
 
-export default Home;
+export default function Home() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid container sm={12}>
+        <Grid container className={classes.heroContainer}>
+          <Grid item sm={6} xs={12} className={classes.heroImgContainer}>
+            PIC
+          </Grid>
+          <Grid item sm={6} xs={12} className={classes.heroContentContainer}>
+            <main>
+              <section>TEXT</section>
+              <section>BUTTONS</section>
+            </main>
+          </Grid>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
